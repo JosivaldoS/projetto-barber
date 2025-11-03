6d030e74bb1ced0fe9d4d6d44764bf765bfad5d9
Josivaldo Soares <josivaldojr365@gmail.com>
2025-11-03 13:28:32 -0300

    Criando a página de Login
    Criei a página de login e criei a rota para ela utilizando as rotas básicas do next.js
    Também atualizei a página para ser em Português do Brasil

---
+++ src/app/login/page.js
@@ -0,0 +1,12 @@
+
+import TelaDeLogin from "@/components/telaDeLogin/telaDeLogin";
+
+export default function Login() {
+    return (
+        <div>
+            <h1>Página de login</h1>
+            <TelaDeLogin/>
+            <a href="/">Voltar para o Home</a>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import TelaDeLogin from "@/components/telaDeLogin/telaDeLogin";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <TelaDeLogin/>
+      <a href="login">Fazer o Login</a>
     </div>
   );
 }
